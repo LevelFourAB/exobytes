309f338698d40674ef85c706b9dd64ff5a2dbcac
Andreas Holstenson <a@holstenson.se>
2020-07-24 12:10:43 +0200

    feat(core): Add AnnotationSerialization

---
+++ exobytes/src/main/java/se/l4/exobytes/AnnotationSerialization.java
@@ -0,0 +1,19 @@
+package se.l4.exobytes;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation that can be used to activate reflection based serializing for
+ * a type.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.TYPE })
+@Documented
+@Use(ReflectionSerializer.class)
+public @interface AnnotationSerialization
+{
+}
