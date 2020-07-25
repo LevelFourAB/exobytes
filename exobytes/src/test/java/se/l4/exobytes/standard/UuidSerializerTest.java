a774d510b8c21d2454ddfe8e1f82ec5f43b62760
Andreas Holstenson <a@holstenson.se>
2020-07-25 09:06:33 +0200

    test(core): Update tests for UUID

--- exobytes/src/test/java/se/l4/exobytes/standard/UuidSerializerTest.java
+++ exobytes/src/test/java/se/l4/exobytes/standard/UuidSerializerTest.java
@@ -1,42 +1,59 @@
 package se.l4.exobytes.standard;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.UUID;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.junit.Test;
 
-import se.l4.exobytes.format.LegacyBinaryInput;
-import se.l4.exobytes.format.LegacyBinaryOutput;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import se.l4.exobytes.Serializer;
+import se.l4.exobytes.SerializerTest;
+import se.l4.exobytes.format.StreamingInput;
+import se.l4.exobytes.format.Token;
 
-public class UuidSerializerTest
-{
+public class UuidSerializerTest extends SerializerTest {
 	@Test
-	public void testOne()
-	{
-		UUID uuid = UUID.fromString("29fa14b7-6fb6-4d68-bec1-40307a949421");
-		UUID second = writeAndRead(uuid);
+	public void testWrite() throws IOException {
+		Serializer<UUID> serializer = new UuidSerializer();
 
-		assertThat(second, is(uuid));
+		UUID value = UUID.fromString("29fa14b7-6fb6-4d68-bec1-40307a949421");
+		StreamingInput in = write(out -> serializer.write(value, out)).get();
+
+		assertThat(in.next(), is(Token.VALUE));
+		assertThat(Hex.encodeHexString(in.readByteArray()), is("29fa14b76fb64d68bec140307a949421"));
 	}
 
-	private UUID writeAndRead(UUID uuid)
+	@Test
+	public void testRead()
+		throws IOException
 	{
-		UuidSerializer serializer = new UuidSerializer();
+		Serializer<UUID> serializer = new UuidSerializer();
 
-		try
-		{
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			serializer.write(uuid, new LegacyBinaryOutput(out));
+		StreamingInput in = write(out -> {
+			try
+			{
+				out.writeBytes(Hex.decodeHex("29fa14b76fb64d68bec140307a949421"));
+			}
+			catch(DecoderException e)
+			{
+				throw new IOException(e);
+			}
+		}).get();
 
-			return serializer.read(new LegacyBinaryInput(new ByteArrayInputStream(out.toByteArray())));
-		}
-		catch(Exception e)
-		{
-			throw new RuntimeException(e);
-		}
+		UUID v = serializer.read(in);
+		assertThat(in.next(), is(Token.END_OF_STREAM));
+
+		assertThat(v, is(UUID.fromString("29fa14b7-6fb6-4d68-bec1-40307a949421")));
+	}
+
+	@Test
+	public void testEquality()
+	{
+		EqualsVerifier.forClass(UuidSerializer.class).verify();
 	}
 }
