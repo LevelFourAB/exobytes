a1656051e10b6a1f55070867c7ec5cc735320c50
Andreas Holstenson <a@holstenson.se>
2021-01-10 09:37:10 +0100

    chore(core): Export all packages

--- exobytes/src/main/java/module-info.java
+++ exobytes/src/main/java/module-info.java
@@ -5,11 +5,16 @@ module se.l4.exobytes {
 	requires transitive se.l4.ylem.types.mapping;
 	requires transitive se.l4.ylem.types.instances;
 
+	requires org.eclipse.collections.api;
+	requires org.eclipse.collections.impl;
+
 	exports se.l4.exobytes;
+	exports se.l4.exobytes.array;
 	exports se.l4.exobytes.collections;
 	exports se.l4.exobytes.enums;
-	exports se.l4.exobytes.streaming;
 	exports se.l4.exobytes.standard;
+	exports se.l4.exobytes.streaming;
+	exports se.l4.exobytes.time;
 
 	uses se.l4.exobytes.SerializersModule;
 }
